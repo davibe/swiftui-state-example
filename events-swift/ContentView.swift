545e4d53e880186f13e2342110a936a169a2a493
Davide Bertola <dade@dadeb.it>
2019-06-06 16:51:17 +0200

    Initial Commit

---
+++ events-swift/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  events-swift
+//
+//  Created by Davide Bertola on 06/06/2019.
+//  Copyright © 2019 Davide Bertola. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
